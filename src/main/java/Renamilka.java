4ff7ad1f18fd5783536ee73cd7afc9f807730edc
IvanOpushkin <farm.vanya@yandex.ru>
2017-10-07 19:37:00 +1000

    Pre load site

--- src/main/java/Renamilka.java
+++ src/main/java/Renamilka.java
@@ -6,19 +6,20 @@ public class Renamilka {
 
     public static void main(String[] argv) throws IOException {
 
-        File folder = new File("C:\\Users\\Mega\\Downloads\\Фотки под сайт (МАЛЕНЬКИЕ)\\PhotoCablesMendeleev");
+        File folder = new File("C:\\Users\\Mega\\Downloads\\ResizedPhotos.0 (1)");
         File[] listOfFiles = folder.listFiles();
 
         for (int i = 0; i < listOfFiles.length; i++) {
 
             if (listOfFiles[i].isFile()) {
 
-                String x = listOfFiles[i].getName().replace("осн","osnova");
+                //Важен порядок запуска, чтобы Осн не переживало на всех фотках
+                String x = listOfFiles[i].getName().replace("основа","osnova");
                x = x.replace(".jpg.jpg.jpg","");
 
-                File f = new File("C:\\Users\\Mega\\Downloads\\Фотки под сайт (МАЛЕНЬКИЕ)\\PhotoCablesMendeleev\\"+listOfFiles[i].getName());
+                File f = new File("C:\\Users\\Mega\\Downloads\\ResizedPhotos.0 (1)\\"+listOfFiles[i].getName());
 
-               f.renameTo(new File("C:\\Users\\Mega\\Downloads\\Фотки под сайт (МАЛЕНЬКИЕ)\\PhotoCablesMendeleev\\"+x));
+               f.renameTo(new File("C:\\Users\\Mega\\Downloads\\ResizedPhotos.0 (1)\\"+x));
             }
         }
 
